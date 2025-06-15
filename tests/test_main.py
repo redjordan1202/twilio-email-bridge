94ac2d052cb2218c59740a92bb213c36c1ed98cb
Jordan Del Pilar <redjordan123@gmail.com>
2025-06-15 12:12:01 -0700

    Fix: Update name for test
    Changed name for test to be more descriptive of actual test

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ from app.core.main import app
 
 test_client = TestClient(app)
 
-def test_twilio_webhook_valid_path():
+def test_twilio_webhook_handles_valid_request():
     response = test_client.post("/webhooks/twilio")
     assert response.status_code == 200
     assert response.json() == {}
