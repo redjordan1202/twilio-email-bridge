130e0ef4baa61e960d72bfbd913dddd2f1b8f077
Jordan Del Pilar <redjordan123@gmail.com>
2025-06-15 11:38:50 -0700

    Add: Test for webhook validation

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,3 +16,36 @@ def test_twilio_webhook_reject_get():
         "description": "Not allowed",
         "message": "Method Not Allowed",
     }
+
+def test_twilio_webhook_raises_error_missing_body_in_form():
+    form_data = {
+        'SmsSid': 'SMxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        'SmsStatus': 'received',
+        'MessageSid': 'SMxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        'AccountSid': 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        'From': '+11234567890',
+        'ApiVersion': '2010-04-01',
+        'SmsMessageSid': 'SMxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        'NumSegments': '1',
+        'To': '+10987654321',
+        'ForwardedFrom': '',
+        'MessageStatus': 'received',
+        # 'Body': 'Hello, World!', # excluding body for test
+        'FromZip': '12345',
+        'FromCity': 'Townsville',
+        'FromState': 'CA',
+        'FromCountry': 'US',
+        'ToZip': '4321',
+        'ToCity': 'Citysberg',
+        'ToState': 'CA',
+        'ToCountry': 'US',
+        'NumMedia': '0',
+    }
+    response = test_client.post("/webhooks/twilio", data=form_data)
+
+    assert response.status_code == 422
+    assert "Validation Error" in response.json()["description"]
+    assert "Field Required" in response.json()["message"]
+    assert "Body" in response.json()["message"]
+    assert len(response.json()["validation_errors"]) > 0
+    assert "body" in response.json()["validation_errors"]
