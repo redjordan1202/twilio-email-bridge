1d21cddfede8028f8edc6d6c9a6012fdbefbc37c
Jordan Del Pilar <redjordan123@gmail.com>
2025-06-15 12:49:33 -0700

    Add: Test that ensures function raises exception on missing credentials

--- tests/test_twilio_logic.py
+++ tests/test_twilio_logic.py
@@ -14,3 +14,10 @@ class TwilioLogicTest(unittest.TestCase):
         from app.exceptions import RequiresClientException
         with self.assertRaises(RequiresClientException):
             get_full_twilio_data("")
+
+
+    def test_get_client_raises_error_on_missing_credentials(self):
+        from app.core.twilio_logic import get_client
+        from app.exceptions import MissingCredentialsException
+        with self.assertRaises(MissingCredentialsException):
+            get_client()
