737282c91f66a1fc66de64a10c93bddadf8c3189
Jordan Del Pilar <redjordan123@gmail.com>
2025-06-15 12:56:50 -0700

    Add: New function to get twilio client.
    Basic functions added to pass tests that test getting twilio credentials

--- app/core/twilio_logic.py
+++ app/core/twilio_logic.py
@@ -1,5 +1,17 @@
+import os
+
+from app.exceptions.exceptions import MissingCredentialsException
 from app.models import TwilioRequest
 from pydantic import ValidationError
+from twilio.rest import Client
+
+
+def get_client():
+    account_sid = os.environ["TWILIO_ACCOUNT_SID"] if "TWILIO_ACCOUNT_SID" in os.environ else None
+    auth_token = os.environ["TWILIO_AUTH_TOKEN"] if "TWILIO_AUTH_TOKEN" in os.environ else None
+    if not account_sid or not auth_token:
+        raise MissingCredentialsException("Required credentials are missing")
+
 
 def extract_message_info(twilio_data: dict) -> dict:
     try:
