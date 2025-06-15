b33de3084ce117cf3d23ba07f1a1d32387618a1c
Jordan Del Pilar <redjordan123@gmail.com>
2025-06-15 13:19:33 -0700

    Update: change from attribute error to keyerror
    Changed to more correct keyerror exception rather than attribute error to match what you would expect for an incorrect or missing key in a function

--- app/core/twilio_logic.py
+++ app/core/twilio_logic.py
@@ -25,6 +25,6 @@ def extract_message_info(twilio_data: dict) -> dict:
     try:
         validated_data = TwilioRequest(**twilio_data)
     except ValidationError as e:
-        raise AttributeError from e
+        raise KeyError from e
 
     return {}
