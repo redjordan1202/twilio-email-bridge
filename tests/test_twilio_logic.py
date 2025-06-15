bceb006f4ec6846c9d86e44178a2503e05f4c248
Jordan Del Pilar <redjordan123@gmail.com>
2025-06-15 13:18:51 -0700

    Update: change attributeerror to keyerror
    Changed to keyerror in test_extract_message_info_raises_error_on_missing_field

--- tests/test_twilio_logic.py
+++ tests/test_twilio_logic.py
@@ -11,7 +11,7 @@ class TwilioLogicTest(unittest.TestCase):
     def test_extract_message_info_raises_error_on_missing_field(self):
         from app.core.twilio_logic import extract_message_info
         twilio_data = {}
-        with self.assertRaises(AttributeError):
+        with self.assertRaises(KeyError):
             extract_message_info(twilio_data)
 
 
