2fbb88774e91d10b1389d66c85b7b6c9aba16c34
Jordan Del Pilar <redjordan123@gmail.com>
2025-06-15 12:48:11 -0700

    Add: Test that ensures that function raises error with client missing

--- tests/test_twilio_logic.py
+++ tests/test_twilio_logic.py
@@ -7,3 +7,10 @@ class TwilioLogicTest(unittest.TestCase):
         twilio_data = {}
         with self.assertRaises(AttributeError):
             extract_message_info(twilio_data)
+
+
+    def test_get_full_twilio_data_raises_error_on_missing_client(self):
+        from app.core.twilio_logic import get_full_twilio_data
+        from app.exceptions import RequiresClientException
+        with self.assertRaises(RequiresClientException):
+            get_full_twilio_data("")
