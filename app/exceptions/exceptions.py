1c0781582489b4a5ae4169bb2bcf31b4877a42e0
Jordan Del Pilar <redjordan123@gmail.com>
2025-06-15 12:55:58 -0700

    Add: New exception to handle missing credentials

---
+++ app/exceptions/exceptions.py
@@ -0,0 +1,13 @@
+class RequiresClientException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+    def __str__(self):
+        return "Twilio client required for function call"
+
+class MissingCredentialsException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+    def __str__(self):
+        return "Twilio credentials are not set as environment variables"
