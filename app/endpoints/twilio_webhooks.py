a2e8bfd93764fa2ddc98b3e32593df3e5f23f943
Jordan Del Pilar <jordan@delpilar.net>
2025-06-16 15:13:04 -0700

    Update: Now created background task and returns reply to twilio before timeout.

--- app/endpoints/twilio_webhooks.py
+++ app/endpoints/twilio_webhooks.py
@@ -1,17 +1,19 @@
-from fastapi import APIRouter, Request, Response, Depends
+from fastapi import APIRouter, Request, BackgroundTasks
 from fastapi.exceptions import RequestValidationError
 from pydantic import ValidationError
 from starlette.responses import JSONResponse
 
 from app.models import TwilioRequest
+from app.core.twilio_logic import twilio_background_task
 
 router = APIRouter()
 
 @router.post("/webhooks/twilio")
-async def handle_twilio_sms(request: Request):
+async def handle_twilio_sms(request: Request, background_tasks: BackgroundTasks = BackgroundTasks):
     data = await request.form()
     try:
         twilio_data = TwilioRequest(**data)
+        background_tasks.add_task(twilio_background_task, request, twilio_data.model_dump())
         return JSONResponse(
             status_code=200,
             content={},
