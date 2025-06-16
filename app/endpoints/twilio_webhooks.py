b972744a714951b32dba5bf69835172c8aa88fae
Jordan Del Pilar <jordan@delpilar.net>
2025-06-16 13:48:54 -0700

    Update: new data validation
    Added data validation to /webhooks/twilio endpoint. Now does validation in endpoint before running anything else.

--- app/endpoints/twilio_webhooks.py
+++ app/endpoints/twilio_webhooks.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter, Request, Response, Depends
+from fastapi.exceptions import RequestValidationError
+from pydantic import ValidationError
+from starlette.responses import JSONResponse
+
 from app.models import TwilioRequest
 
 router = APIRouter()
 
 @router.post("/webhooks/twilio")
-def handle_twilio_sms(twilio_request: TwilioRequest = Depends()):
-    pass
+async def handle_twilio_sms(request: Request):
+    data = await request.form()
+    try:
+        twilio_data = TwilioRequest(**data)
+        return JSONResponse(
+            status_code=200,
+            content={},
+        )
+    except ValidationError as e:
+        raise RequestValidationError(
+            errors=e.errors(),
+        )
