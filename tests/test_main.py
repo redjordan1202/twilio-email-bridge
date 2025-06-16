2c35d4cc9dca57f280b509a3de309c6a34c1a3f6
Jordan Del Pilar <jordan@delpilar.net>
2025-06-16 15:12:06 -0700

    Update: updated test to now check for background task
    Test now expects that the webhook can be called with valid data, and asserts that the background task is correctly added.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,8 @@
+import unittest
+from unittest.mock import patch, ANY
 from fastapi.testclient import TestClient
 from app.core.main import app
+from app.core.twilio_logic import twilio_background_task
 import json
 
 test_client = TestClient(app)
@@ -17,14 +20,25 @@ dummy_message = {
     "ForwardedFrom": "",
     "MessageStatus": "received",
     "Body": "Hello World",
+    "FromZip": "",
+    "FromCity":  "",
+    "FromState": "",
+    "FromCountry": "",
+    "ToZip": "",
+    "ToCity": "",
+    "ToState": "",
+    "ToCountry": "",
     "NumMedia": "0"
 }
 
-def test_twilio_webhook_handles_valid_request():
+@patch('fastapi.BackgroundTasks.add_task')
+def test_twilio_webhook_handles_valid_request(mock_add_task):
     response = test_client.post("/webhooks/twilio", data=dummy_message)
     print(response.request.content)
     assert response.status_code == 200
     assert response.json() == {}
+    mock_add_task.assert_called_once()
+    mock_add_task.assert_called_once_with(twilio_background_task, ANY, dummy_message)
 
 def test_twilio_webhook_reject_get():
     response = test_client.get("/webhooks/twilio")
