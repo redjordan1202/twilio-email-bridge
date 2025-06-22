8277cedca84c964d57248e3b3b6bc45226ca05ed
Jordan Del Pilar <redjordan123@gmail.com>
2025-06-21 22:12:08 -0700

    Add: Logging and eror handling

--- app/core/twilio_logic.py
+++ app/core/twilio_logic.py
@@ -1,4 +1,6 @@
 import os
+import logging
+from datetime import datetime
 
 from twilio.base.exceptions import TwilioRestException
 from twilio.request_validator import RequestValidator
@@ -8,6 +10,8 @@ from fastapi import Request, Form
 from app.exceptions.exceptions import MissingCredentialsException, ClientAuthenticationException, \
     RequiresClientException, ResourceNotFoundException, InvalidTwilioRequestException
 from twilio.rest import Client
+
+from app.models import LogEntry
 
 
 def get_client() -> Client:
@@ -85,12 +89,53 @@ def sanitize_data(data: dict) -> dict:
 
 def twilio_background_task(request: Request, data: dict) -> dict:
     if not validate_twilio_request(request, data):
+        failure_log = LogEntry(
+            timestamp=datetime.now(),
+            level="ERROR",
+            message="Twilio request is invalid",
+            service_name="Twilio Webhook",
+            trace_id=request.headers.get("X-Twilio-Trace-ID", "None"),
+            context={
+                "message_sid": data.get("MessageSid", "")
+            }
+        )
+        logging.error(failure_log.to_json())
         raise InvalidTwilioRequestException("Invalid Twilio request")
 
-    client = get_client()
-    msg_sid = data.get("MessageSid")
-    if not msg_sid:
-        raise ValueError("MessageSid is required in the data")
-    full_twilio_data = get_full_twilio_data(client, msg_sid)
-    extracted_info = extract_message_info(full_twilio_data)
-    return extracted_info
+    try:
+        client = get_client()
+        msg_sid = data.get("MessageSid")
+        if not msg_sid:
+            raise ValueError("MessageSid is required in the data")
+        full_twilio_data = get_full_twilio_data(client, msg_sid)
+        extracted_info = extract_message_info(full_twilio_data)
+        success_log = LogEntry(
+            timestamp=datetime.now(),
+            level="INFO",
+            message="SMS Processed Successfully",
+            service_name="Twilio Webhook",
+            trace_id=request.headers.get("X-Twilio-Trace-ID", "None"),
+            context=sanitize_data(data),
+        )
+        logging.info(success_log.to_json())
+        return extracted_info
+    except (
+            MissingCredentialsException,
+            ClientAuthenticationException,
+            RequiresClientException,
+            ResourceNotFoundException
+    ) as e:
+        try:
+            sanitized_data = sanitize_data(data)
+        except ValueError:
+            sanitized_data = {"MessageSid": "MessageSid Not Found"}
+        failure_log = LogEntry(
+            timestamp=datetime.now(),
+            level="ERROR",
+            message= str(e),
+            service_name="Twilio Webhook",
+            trace_id=request.headers.get("X-Twilio-Trace-ID", "None"),
+            context=sanitized_data,
+        )
+        logging.error(failure_log.to_json())
+        raise e
