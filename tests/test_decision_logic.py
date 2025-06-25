1531e427d162622f2a65d8997324945375ebfcb2
redjordan <jordan@delpilar.net>
2025-06-25 13:51:31 -0700

    Add: New test suite for testing get_routes function
    New tests confirm all possible outcomes of routing messages.

--- tests/test_decision_logic.py
+++ tests/test_decision_logic.py
@@ -2,6 +2,7 @@ import unittest
 from datetime import datetime
 
 from app.exceptions import RouteProcessingError
+from app.decision_logic import get_routes
 
 
 class TestDecisionLogic(unittest.TestCase):
@@ -17,6 +18,23 @@ class TestDecisionLogic(unittest.TestCase):
             "from": "+11234567890",
             "body": "[CRITICAL] - Fake Server Alert",
             "routes": ["email", "text", "discord"]
+        }
+
+        actual_result = get_routes(dummy_message)
+        self.assertEqual(expected_result, actual_result)
+
+
+    def test_get_routes_returns_correct_route_for_warning_alerts(self):
+        dummy_message = {
+            "date_created": datetime.now(),
+            "from": "+11234567890",
+            "body": "[WARNING] - Fake Server Alert",
+        }
+        expected_result = {
+            "date_created": dummy_message["date_created"],
+            "from": "+11234567890",
+            "body": "[WARNING] - Fake Server Alert",
+            "routes": ["email", "discord"]
         }
 
         actual_result = get_routes(dummy_message)
