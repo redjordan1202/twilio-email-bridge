3d0b5ac05bb8c9d5c5674cc2456909f18804d9fe
redjordan <jordan@delpilar.net>
2025-07-15 11:26:59 -0700

    Refactor: Added setUp method to handle mocking
    Added setUp method to handle mocking to create baseline set of mocked objects to allow for all test to create instances of the EmailSender class. Also handles enviroment variables and sets them a standard test set of variables

--- tests/test_email_sender.py
+++ tests/test_email_sender.py
@@ -13,45 +13,29 @@ from app.exceptions import MissingCredentialsException, GoogleAuthError
 
 
 class TestEmailSender(unittest.TestCase):
-    @patch.dict(os.environ, {}, clear=True)
-    @patch("app.email_sender.email_sender.service_account.Credentials")
-    @patch("app.email_sender.email_sender.build")
-    def test_init_raises_exception_with_missing_envars(self, mock_build, mock_credentials):
-        with self.assertRaises(MissingCredentialsException):
-            sender = EmailSender()
-        mock_build.assert_not_called()
-        mock_credentials.from_service_account_info.assert_not_called()
+    def setUp(self):
 
-    @patch.dict(
-        os.environ,
-        {
+        # Setup mock environment variables
+        self.mock_env = {
             "DELEGATED_USER_EMAIL": "test@test.com",
             "PROJECT_ID": "id-1234",
             "SECRET_NAME": "test_secret_name"
         }
-    )
-    @patch("app.email_sender.email_sender.service_account.Credentials")
-    @patch("app.email_sender.email_sender.build")
-    def test_init_raises_exception_on_auth_error(self, mock_build, mock_credentials):
-        mock_credentials.from_service_account_info.side_effect = ValueError
-        with self.assertRaises(GoogleAuthError):
-            sender = EmailSender()
-        mock_build.assert_not_called()
 
-    @patch.dict(
-        os.environ,
-        {
-            "DELEGATED_USER_EMAIL": "test@test.com",
-            "PROJECT_ID": "id-1234",
-            "SECRET_NAME": "test_secret_name"
-        }
-    )
-    @patch("app.email_sender.email_sender.service_account.Credentials")
-    @patch("app.email_sender.email_sender.build")
-    @patch("app.email_sender.email_sender.secretmanager.SecretManagerServiceClient")
-    def test_init_creates_service_account(self, mock_secret_manager, mock_build, mock_credentials):
+        self.patcher_environ = patch.dict('os.environ', self.mock_env)
+        self.patcher_environ.start()
+        self.addCleanup(self.patcher_environ.stop)
 
-        mock_credentials_file = {
+        # Setup mock objects needed for EmailSender class initialization
+        patcher_credentials = patch("app.email_sender.email_sender.service_account.Credentials")
+        patcher_build = patch("app.email_sender.email_sender.build")
+        patcher_secret_manager = patch("app.email_sender.email_sender.secretmanager.SecretManagerServiceClient")
+
+        self.mock_credentials = patcher_credentials.start()
+        self.mock_build = patcher_build.start()
+        self.mock_secret_manager = patcher_secret_manager.start()
+
+        self.mock_credentials_file = {
             "type": "service_account",
             "project_id": "project-id-1234",
             "private_key_id": "private-key-id-1234",
@@ -64,31 +48,52 @@ class TestEmailSender(unittest.TestCase):
             "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509",
             "universe_domain": "googleapis.com"
         }
-        json_str = json.dumps(mock_credentials_file)
+        json_str = json.dumps(self.mock_credentials_file)
         secret_bytes = bytes(json_str, "UTF-8")
         payload = SecretPayload(data=secret_bytes)
         mock_response = AccessSecretVersionResponse(payload=payload)
-        mock_secret_client_instance = mock_secret_manager.return_value
+
+        mock_secret_client_instance = self.mock_secret_manager.return_value
         mock_secret_client_instance.access_secret_version.return_value = mock_response
 
-        mock_credentials_instance = MagicMock()
-        mock_delegated_credentials_instance = MagicMock()
-        mock_service_account = MagicMock(spec=Resource)
+        self.mock_credentials_instance = MagicMock()
+        self.mock_delegated_credentials_instance = MagicMock()
+        self.mock_service_account = MagicMock(spec=Resource)
 
-        mock_credentials.from_service_account_info.return_value = mock_credentials_instance
-        mock_credentials_instance.with_subject.return_value = mock_delegated_credentials_instance
-        mock_build.return_value = mock_service_account
+        self.mock_credentials.from_service_account_info.return_value = self.mock_credentials_instance
+        self.mock_credentials_instance.with_subject.return_value = self.mock_delegated_credentials_instance
+        self.mock_build.return_value = self.mock_service_account
+
+        self.addCleanup(patcher_credentials.stop)
+        self.addCleanup(patcher_build.stop)
+        self.addCleanup(patcher_secret_manager.stop)
+
+    @patch.dict(os.environ, {}, clear=True)
+    def test_init_raises_exception_with_missing_envars(self):
+        with self.assertRaises(MissingCredentialsException):
+            sender = EmailSender()
+        self.mock_build.assert_not_called()
+        self.mock_credentials.from_service_account_info.assert_not_called()
+
+    @patch("app.email_sender.email_sender.service_account.Credentials")
+    @patch("app.email_sender.email_sender.build")
+    def test_init_raises_exception_on_auth_error(self, mock_build, mock_credentials):
+        mock_credentials.from_service_account_info.side_effect = ValueError
+        with self.assertRaises(GoogleAuthError):
+            sender = EmailSender()
+        mock_build.assert_not_called()
+
+    def test_init_creates_service_account(self):
 
         sender = EmailSender()
 
-        mock_credentials.from_service_account_info.assert_called_once_with(
-            mock_credentials_file,
+        self.mock_credentials.from_service_account_info.assert_called_once_with(
+            self.mock_credentials_file,
             scopes=["https://www.googleapis.com/auth/gmail.send"]
         )
-        mock_credentials_instance.with_subject.assert_called_once_with("test@test.com")
-        mock_build.assert_called_once_with("gmail", "v1", credentials=mock_delegated_credentials_instance)
-        self.assertIs(sender.service, mock_service_account)
-
+        self.mock_credentials_instance.with_subject.assert_called_once_with("test@test.com")
+        self.mock_build.assert_called_once_with("gmail", "v1", credentials=self.mock_delegated_credentials_instance)
+        self.assertIs(sender.service, self.mock_service_account)
 
     def test_build_email_requires_destination(self):
         with self.assertRaises(ValueError) as e:
@@ -110,7 +115,6 @@ class TestEmailSender(unittest.TestCase):
             email = EmailSender.build_email("toemail.com", "Body", "Subject")
         self.assertEqual(str(e.exception), "Invalid address")
 
-
     def test_build_email_returns_encoded_email(self):
         to = "to@email.com"
         body = "Body"
@@ -126,9 +130,3 @@ class TestEmailSender(unittest.TestCase):
         self.assertEqual(to, parsed_result["to"])
         self.assertEqual(body, parsed_result.get_payload())
         self.assertEqual(subject, parsed_result["subject"])
-
-
-
-
-
-
