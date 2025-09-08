cb61ee0639a6ffd0d8eecdb4e7daf18da7035401
redjordan <jordan@delpilar.net>
2025-09-08 11:32:43 -0700

    Remove: Removed un-needed test
    test_background_task_function_logs_error_on_invalid_request removed as it is no longer needed to new validator set up.

--- tests/test_twilio_logic.py
+++ tests/test_twilio_logic.py
@@ -176,19 +176,6 @@ class TwilioLogicTest(unittest.TestCase):
         mock_sender_instance.send_email.assert_called_once()
 
     @patch('app.core.twilio_logic.validate_twilio_request')
-    @patch('app.core.twilio_logic.logging.error')
-    def test_background_task_function_logs_error_on_invalid_request (self,mock_logger, mock_validate_twilio_request):
-        mock_validate_twilio_request.return_value = False
-        mock_request = MagicMock()
-        mock_request.url.path = '/webhooks/twilio'
-        mock_request.headers = {'X-Twilio-Signature': 'fake_signature'}
-        mock_data = {'MessageSid': 'fake_msg_sid', 'key': 'value'}
-
-        twilio_background_task(mock_request, mock_data)
-        mock_logger.assert_called_once()
-
-
-    @patch('app.core.twilio_logic.validate_twilio_request')
     @patch('app.core.twilio_logic.get_client')
     @patch('app.core.twilio_logic.logging.error')
     def test_background_task_function_logs_error_when_exception_is_raised(
