7f522a271681a358820b3fbff477eedd3dbaa977
redjordan <jordan@delpilar.net>
2025-09-25 09:57:33 -0700

    update: Updated desicion logic test suite
    Updated test suite for the decision logic in preperation for upcoming chage to using DecisionLogic class for future use.

--- tests/test_decision_logic.py
+++ tests/test_decision_logic.py
@@ -6,6 +6,32 @@ from app.decision_logic import get_routes
 
 
 class TestDecisionLogic(unittest.TestCase):
+    def setUp(self):
+        self.dummy_config = {
+            "plugins": [
+                {
+                    "name": "default_email_sender",
+                    "type": "smtp_sender",
+                    "config": {
+                        "recipient_email": "user@domain.com",
+                        "smtp_server": "smtp.gmail.com",
+                        "smtp_port": 587
+                    }
+                }
+            ],
+            "severity_levels": {
+                    "normal": ["default_email_sender"],
+                    "urgent": ["urgent_email_sender"],
+                    "critical": ["critical_email_sender"],
+                    "mfa": ["mfa_email_sender"]
+            }
+        }
+
+        self.dummy_message = {
+            'body': "Test Message",
+            'from': "5551234567",
+            'date_created': datetime.now()
+        }
 
     def test_get_routes_returns_correct_route_for_critical_alerts(self):
         dummy_message = {
@@ -17,14 +43,15 @@ class TestDecisionLogic(unittest.TestCase):
             "date_created": dummy_message["date_created"],
             "from": "+11234567890",
             "body": "[CRITICAL] - Fake Server Alert",
-            "routes": ["email", "text", "discord"]
+            "routes": ["critical_email_sender"]
         }
 
-        actual_result = get_routes(dummy_message)
+        decision_logic = DecisionLogic(message = dummy_message, config = self.dummy_config)
+        actual_result = decision_logic.get_routes(dummy_message)
         self.assertEqual(expected_result, actual_result)
 
 
-    def test_get_routes_returns_correct_route_for_warning_alerts(self):
+    def test_get_routes_returns_correct_route_for_urgent_alerts(self):
         dummy_message = {
             "date_created": datetime.now(),
             "from": "+11234567890",
@@ -34,13 +61,14 @@ class TestDecisionLogic(unittest.TestCase):
             "date_created": dummy_message["date_created"],
             "from": "+11234567890",
             "body": "[WARNING] - Fake Server Alert",
-            "routes": ["email", "discord"]
+            "routes": ["urgent_email_sender"]
         }
 
-        actual_result = get_routes(dummy_message)
+        decision_logic = DecisionLogic(message = dummy_message, config = self.dummy_config)
+        actual_result = decision_logic.get_routes(dummy_message)
         self.assertEqual(expected_result, actual_result)
 
-    def test_get_routes_returns_correct_route_for_2fa_messages(self):
+    def test_get_routes_returns_correct_route_for_mfa_messages(self):
         dummy_message = {
             "date_created": datetime.now(),
             "from": "+11234567890",
@@ -50,13 +78,14 @@ class TestDecisionLogic(unittest.TestCase):
             "date_created": dummy_message["date_created"],
             "from": "+11234567890",
             "body": "code 123456",
-            "routes": ["email", "text"]
+            "routes": ["mfa_email_sender"]
         }
 
-        actual_result = get_routes(dummy_message)
+        decision_logic = DecisionLogic(message = dummy_message, config = self.dummy_config)
+        actual_result = decision_logic.get_routes(dummy_message)
         self.assertEqual(expected_result, actual_result)
 
-    def test_get_routes_returns_correct_route_for_regular_messages(self):
+    def test_get_routes_returns_correct_route_for_normal_messages(self):
         dummy_message = {
             "date_created": datetime.now(),
             "from": "+11234567890",
@@ -66,10 +95,11 @@ class TestDecisionLogic(unittest.TestCase):
             "date_created": dummy_message["date_created"],
             "from": "+11234567890",
             "body": "Hello World!",
-            "routes": ["email"]
+            "routes": ["default_email_sender"]
         }
 
-        actual_result = get_routes(dummy_message)
+        decision_logic = DecisionLogic(message = dummy_message, config = self.dummy_config)
+        actual_result = decision_logic.get_routes(dummy_message)
         self.assertEqual(expected_result, actual_result)
 
     def test_get_routes_raise_exception_on_processing_error(self):
@@ -79,4 +109,53 @@ class TestDecisionLogic(unittest.TestCase):
             "body": None,
         }
         with self.assertRaises(RouteProcessingError):
-            get_routes(dummy_message)
+            decision_logic = DecisionLogic(message = dummy_message, config = self.dummy_config)
+            decision_logic.get_routes(dummy_message)
+
+    def test_init_function_raises_error_on_missing_message_data(self):
+        with self.assertRaises(TypeError):
+            decision_logic = DecisionLogic(config = self.dummy_config)
+
+    def test_init_function_raises_error_on_missing_config(self):
+        with self.assertRaises(TypeError):
+            decision_logic = DecisionLogic(message = self.dummy_message)
+
+    def test_decision_logic_raises_error_on_misisng_severity_section(self):
+        dummy_config = {
+            "plugins": [
+                {
+                    "name": "default_email_sender",
+                    "type": "smtp_sender",
+                    "config": {
+                        "recipient_email": "user@domain.com",
+                        "smtp_server": "smtp.gmail.com",
+                        "smtp_port": 587
+                    }
+                }
+            ]
+        }
+        with self.assertRaises(MissingServeritySectionError):
+            decision_logic = DecisionLogic(message = self.dummy_message, config = dummy_config)
+
+    def test_decision_logic_raises_error_on_missing_severity_level(self):
+        dummy_config = {
+            "plugins": [
+                {
+                    "name": "default_email_sender",
+                    "type": "smtp_sender",
+                    "config": {
+                        "recipient_email": "user@domain.com",
+                        "smtp_server": "smtp.gmail.com",
+                        "smtp_port": 587
+                    }
+                }
+            ],
+            "severity_levels": {
+                    "normal": ["default_email_sender"],
+                    "urgent": ["default_email_sender"],
+                    "critical": ["default_email_sender"]
+            }
+        }
+
+        with self.assertRaises(MissingServerityLevelsError):
+            decision_logic = DecisionLogic(message = self.dummy_message, config = dummy_config)
